835e9d73edc6228c42b46006b4dd2ca2806204fc
Yishai Landau <yishail@gmail.com>
2015-06-30 18:44:56 +0300

    Starting to write directives for each input type

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ var app = angular.module('COMET', ['ngMaterial']);
 
 app.controller('appCtrl', ['$scope', '$mdSidenav', '$http', function ($scope, $mdSidenav, $http) {
 	$http.get("json_src/pilot.json").then(function fileLoaded(res){
-		console.log("File loaded");
 		$scope.formData = res.data;
 	});
 	$scope.toggleSidenav = function(menuId){
@@ -14,4 +13,22 @@ app.controller('appCtrl', ['$scope', '$mdSidenav', '$http', function ($scope, $m
 .config(function($mdThemingProvider) {
   $mdThemingProvider.theme('altTheme')
     .primaryPalette('purple')
-});
+})
+.directive('cometInt', function(){
+	return {
+		template: "<input type='text' ng-minlength='1' ng-maxlength='3' name='ACBX' value='' />"
+	}
+})
+.directive('cometText', [function () {
+	return {
+		restrict: 'E',
+		scope: {
+			formField: '=field'
+		},
+		templateUrl: function(elem, attr){
+			return 'tpl/input_tpl.html';
+		}
+	};
+}])
+
+;
