a3f2a79ee8b4fe642993468c87068d311273b8c3
Ivan Parmaclii <ivan.parmaclii19@gmail.com>
2016-03-08 09:22:31 +0200

    fix modal controller injection

--- js/controllers/modalInstanceCtrl.js
+++ js/controllers/modalInstanceCtrl.js
@@ -1,37 +1,35 @@
 // app.controller('modalController',['$scope', 'close','loadPath', 'formTitle', '$element', function($scope, close, loadPath, formTitle, $element){
-	
-// 	$scope.loadPath = loadPath;
-// 	//$scope.formElements = data.form[0];
-// 	$scope.title = formTitle;
 
-// 	$scope.dismissModal = function(res){
-// 		close(res, 200);
-// 		//angular modal clashes with bootstrap modal. The following lines take care of bootstrap
-// 		$(".modal-backdrop").remove();
-// 		$("body").removeClass("modal-open");
-// 	}
+// $scope.loadPath = loadPath;
+// //$scope.formElements = data.form[0];
+// $scope.title = formTitle;
+
+// $scope.dismissModal = function(res){
+//  close(res, 200);
+//  //angular modal clashes with bootstrap modal. The following lines take care of bootstrap
+//  $(".modal-backdrop").remove();
+//  $("body").removeClass("modal-open");
+// }
 
 // }]);
 
-app  = angular.module('COMET');
-var $ = require('jquery');
+var app  = angular.module('COMET');
 
-app.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, loadPath) {
+app.controller('ModalInstanceCtrl',['$scope', '$uibModalInstance', 'loadPath',
+    function ($scope, $uibModalInstance, loadPath) {
 
- 
 
-  $scope.loadPath = loadPath;
-  
-  console.log($scope);
-  $scope.ok = function () {
-    $uibModalInstance.close($scope.selected.item);
-  };
+    $scope.loadPath = loadPath;
 
-  $scope.cancel = function () {
-    $uibModalInstance.dismiss('cancel');
-  };
+    $scope.ok = function () {
+        $uibModalInstance.close($scope.selected.item);
+    };
 
-  $scope.getPath = function(){
-  	return loadPath;
-  }
-});
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+
+    $scope.getPath = function(){
+        return $scope.loadPath;
+    };
+}]);
