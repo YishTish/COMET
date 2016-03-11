a84bdd8a1c9c2e9d74c755ad72cf63693438a989
Ivan Parmaclii <ivan.parmaclii19@gmail.com>
2016-03-11 16:09:44 +0200

    fix the menu layout

--- js/services/menuServices.js
+++ js/services/menuServices.js
@@ -80,23 +80,58 @@ app.factory('menuServices', [function () {
 		data.forEach(function(category) {
 			var list = [];
 			for (var i = 0; i < +category.columns; i++) list[i] = [];
-			category.groups.forEach(function (group) {
+			var prevGroup = null;
+
+			category.groups.sort(function compare(one, two) {
+				// when groups are in the same column, we sort by row
+				if (+one.column === +two.column) {
+						return +one.row - +two.row;
+				}
+
+				return +two.column - +one.column;
+			});
+
+			category.groups.forEach(function (group, index) {
 				// we don't want to display empty groups
 				if (group.items.length > 0 && group.column) {
 
+					// place the group to the 'row' attribute by adding empty lines
+					addEmptyRows(list[+group.column - 1], group, prevGroup,  index);
 					// add group to the column
-					addEmptyRows(list[+group.column - 1], group);
 					list[+group.column - 1].push(group);
 
 					// add each item of the group to the column
 					group.items.forEach(function (item) {
 						list[+group.column - 1].push(item);
 					});
+					prevGroup = group;
 				}
-				function addEmptyRows(list, row) {
-					if (!(list.length > 0 && row.row)) return;
-					var emptyLines = +row.row - +list[list.length - 1].row;
-					for (var i = 0; i < emptyLines - 1; i++) { list.push({ empty: true }); }
+				function addEmptyRows(list, cur, prev, index) {
+					var emptyLines;
+					// if it is first group, there is no prev, and we have to check row
+					if (!prev && cur.row) {
+						emptyLines = +cur.row;
+						// we add empty lines 'row' times
+						addLines(list, emptyLines);
+						return;
+						// if there wasn't row we cannot continue
+					} else if (!cur.row) return;
+					// if there was no title in the fisrt group we make prev.row irrelevant
+					if (!prev.row) prev.row = 0;
+					// in cases where column is not defined, but there are items
+					if (!prev.column) prev.column = 1;
+
+					if (+prev.column === +cur.column) {
+						emptyLines = +cur.row - (prev.items.length + +prev.row);
+					} else {
+						emptyLines = +cur.row;
+					}
+
+					addLines(list, emptyLines);
+
+					function addLines(list, lines) {
+						for (var i = 0; i < lines - 1; i++) { list.push({ empty: true }); }
+					}
 				}
 			});
 
